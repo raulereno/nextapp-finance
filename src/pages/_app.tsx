d400fa79e609b7cf10da41c3a903d7b635e02237
Juan Lopez <juanpablolopez43@gmail.com>
2023-03-24 10:35:46 -0300

    configuration Bootstrap and continue with NavBar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css'
+import '@/styles/styles.css'
 import type { AppProps } from 'next/app'
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
--- src/src-client/components/Test.tsx
+++ src/src-client/components/Test.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Nav from 'react-bootstrap/Nav';
@@ -10,8 +9,8 @@ function Test() {
   const handleShow = () => setShow(true);
 
   return (
-    <div className='w-100% h-screen d-flex'>
-      <Button className='' onClick={handleShow}>
+    <div className='bg-DarkBlue'>
+      <Button className='bg-DarkBlue' onClick={handleShow}>
         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
           <path d="M4 6l16 0"></path>
@@ -20,7 +19,7 @@ function Test() {
         </svg>
       </Button>
 
-      <Offcanvas show={show} onHide={handleClose}>
+      <Offcanvas show={show} onHide={handleClose} className="bg-DarkBlue">
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Offcanvas</Offcanvas.Title>
         </Offcanvas.Header>
