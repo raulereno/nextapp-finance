c30090acbe5310c2790fef89057f56a7f54169c0
Juan Lopez <juanpablolopez43@gmail.com>
2023-03-24 20:01:56 -0300

    NavBar actualized

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Test from '@/src-client/components/Test'
+import NavBar from '@/src-client/components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main >
-        <Test />
+        <NavBar page='home'/>
       </main>
     </>
   )
