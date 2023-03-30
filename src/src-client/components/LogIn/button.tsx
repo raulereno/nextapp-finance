4773ca1d0abcda4d1e9a1e72aa7cb970901a96aa
Jhony Saenz <jash0310@gmail.com>
2023-03-29 21:27:36 -0500

    organizando pagina account

--- src/src-client/components/LogIn/button.tsx
+++ src/src-client/components/LogIn/button.tsx
@@ -5,17 +5,21 @@ const LogButton = () => {
   const { data: session } = useSession();
   if (session && session.user) {
     return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
+      <div className="d-flex align-items-center">
+        <span className="me-2">
+          Signed in as {session.user.email}
+        </span>
+        <button className="btn btn-primary" onClick={() => signOut()}>Sign out</button>
+      </div>
     );
   }
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn("auth0")}>Sign in</button>
-    </>
+    <div className="d-flex align-items-center">
+      <span className="me-2">
+        Not signed in
+      </span>
+      <button className="btn btn-outline-primary" onClick={() => signIn("auth0")}>Sign in</button>
+    </div>
   );
 };
 
